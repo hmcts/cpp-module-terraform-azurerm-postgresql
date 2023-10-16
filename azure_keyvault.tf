6d2f887b63a97f2985028c071d4c805f73c1d0d3
omerf-hmcts <omer.faruk@hmcts.net>
2023-10-16 14:56:05 +0000

    WIP

--- azure_keyvault.tf
+++ azure_keyvault.tf
@@ -2,6 +2,7 @@
 // data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault" "keyvault" {
+  count               = var.enable_azure_keyvault ? 1 : 0
   name                = var.keyvault_name
   resource_group_name = var.keyvault_resource_group_name
 }
@@ -10,5 +11,5 @@ resource "azurerm_key_vault_secret" "postgresql" {
   count        = var.enable_azure_keyvault ? 1 : 0
   name         = var.administrator_creds_azkv_secret_name
   value        = data.vault_generic_secret.administrator_creds.data["administrator_password"]
-  key_vault_id = data.azurerm_key_vault.keyvault.id
+  key_vault_id = data.azurerm_key_vault.keyvault[0].id
 }
