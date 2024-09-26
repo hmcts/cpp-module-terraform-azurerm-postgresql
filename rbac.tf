a9d5ce54abe4010097b764c7b0217dbc526bca08
varunvijayagopal <varun.vijayagopal@hmcts.net>
2024-09-26 12:28:28 +0100

    rbac support

--- rbac.tf
+++ rbac.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "db_setup" {
   for_each = var.rbac
   triggers = {
-    content = templatefile("${path.module}/roles/${each.value.file}", { groups = each.value.groups })
+    content = templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] })
   }
   depends_on = [azurerm_postgresql_flexible_server.flexible_server, azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin]
 
   provisioner "file" {
-    content     = templatefile("${path.module}/roles/${each.value.file}", { groups = each.value.groups })
+    content     = templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] })
     destination = "${path.module}/roles/final_${each.value.file}"
   }
   provisioner "local-exec" {
