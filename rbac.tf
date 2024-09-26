d21437e7d47247b119434ea37c381d6b3577df5f
varunvijayagopal <varun.vijayagopal@hmcts.net>
2024-09-26 13:27:50 +0100

    rbac support

--- rbac.tf
+++ rbac.tf
@@ -5,19 +5,15 @@ resource "null_resource" "db_setup" {
   }
   depends_on = [azurerm_postgresql_flexible_server.flexible_server, azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin]
 
-  #  provisioner "file" {
-  #    content     = templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] })
-  #    destination = "${path.module}/roles/final_${each.value.file}"
-  #  }
   provisioner "local-exec" {
     command = <<EOT
-      echo templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] }) > ${path.module}/roles/final_${each.value.file}
-      az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id}
+      echo "$render_template" > ${path.module}/roles/final_${each.value.file}
+      az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
       export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
       psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.name} -p 5432 -U ${var.entra_admin_user} -d postgres -f ${path.module}/roles/final_${each.value.file}
     EOT
     environment = {
-      PGPASSWORD = "${data.azurerm_key_vault_secret.entra_admin.0.value}"
+      render_template = templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] })
     }
   }
 }
