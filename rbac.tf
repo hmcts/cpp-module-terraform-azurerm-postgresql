7b5c404a9f9038b0c68bb795a21ba4ca8f990084
varunvijayagopal <varun.vijayagopal@hmcts.net>
2024-09-26 14:03:25 +0100

    rbac support

--- rbac.tf
+++ rbac.tf
@@ -10,7 +10,7 @@ resource "null_resource" "db_setup" {
       echo "$render_template" > ${path.module}/roles/final_${each.value.file}
       az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
       export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
-      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -f ${path.module}/roles/final_${each.value.file}
+      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/final_${each.value.file}
     EOT
     environment = {
       render_template = templatefile("${path.module}/roles/${each.value.file}", { groups = [for group in each.value.groups : lower(group)] })
