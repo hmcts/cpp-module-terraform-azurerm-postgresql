7e62876d449fc3f970310c974b79b2d276a92f5a
Jyothi Balla <jyothi.balla@hmcts.net>
2024-10-16 12:06:48 +0100

    update privileges

--- roles/editor.sql
+++ roles/editor.sql
@@ -11,8 +11,8 @@ END
 $do$;
 
 --read only:
-grant select, insert, update, delete, create on all tables in schema public to ${group};
+grant select, insert, update, delete, create, connect on all tables in schema public to ${group};
 grant usage, select, update on all sequences in schema public to ${group};
-alter default privileges in schema public grant select, insert, update, delete, create on tables to ${group};
+alter default privileges in schema public grant select, insert, update, delete, create, connect on tables to ${group};
 alter default privileges in schema public grant usage, select, update on sequences to ${group};
  %{~ endfor ~}
