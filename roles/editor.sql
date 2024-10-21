8612690cd9262e7b01eb704b530a65894e42b509
jyothi-balla <131781883+jyothi-balla@users.noreply.github.com>
2024-10-21 09:51:47 +0100

    Ei2167: Editor privilege sql file (#27)
    * Editor role for postgres users
    
    * update privileges
    
    * update comment
    
    * cleanup
    
    * cleanup

---
+++ roles/editor.sql
@@ -0,0 +1,19 @@
+%{~ for group in groups ~}
+--creating all the required roles for AAD groups
+DO
+$do$
+BEGIN
+  IF NOT EXISTS (
+    SELECT FROM pg_catalog.pg_roles WHERE  rolname = '${group}') THEN
+      PERFORM * from pgaadauth_create_principal('${group}', false, false);
+  END IF;
+END
+$do$;
+
+--Editor privileges:
+grant select, insert, update on all tables in schema public to ${group};
+grant usage, select, update on all sequences in schema public to ${group};
+
+alter default privileges in schema public grant select, insert, update on tables to ${group};
+alter default privileges in schema public grant usage, select, update on sequences to ${group};
+%{~ endfor ~}
