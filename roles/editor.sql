e7d7e2fdb1e7ca2fdf77a4ba003e1392b6860853
Jyothi Balla <jyothi.balla@hmcts.net>
2024-10-21 09:25:22 +0100

    cleanup

--- roles/editor.sql
+++ roles/editor.sql
@@ -1,4 +1,4 @@
-%{~ for group in groups ~}
+
 --creating all the required roles for AAD groups
 DO
 $do$
@@ -11,8 +11,9 @@ END
 $do$;
 
 --Editor privileges:
-grant select, insert, update, delete, create, connect on all tables in schema public to ${group};
+grant select, insert, update on all tables in schema public to ${group};
 grant usage, select, update on all sequences in schema public to ${group};
-alter default privileges in schema public grant select, insert, update, delete, create, connect on tables to ${group};
+
+alter default privileges in schema public grant select, insert, update on tables to ${group};
 alter default privileges in schema public grant usage, select, update on sequences to ${group};
- %{~ endfor ~}
+
