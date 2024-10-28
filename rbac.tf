da1c6fc4176866cb20458c45fe717fa5d97602db
Jyothi Balla <jyothi.balla@hmcts.net>
2024-10-28 15:13:15 +0000

    unique

--- rbac.tf
+++ rbac.tf
@@ -11,7 +11,29 @@ resource "null_resource" "db_setup" {
       echo "$render_template" > ${path.module}/roles/$unique_sql_file_name
       az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
       export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
-      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/$unique_sql_file_name
+      max_retries=5
+      count=0
+      success=0
+      while [ $count -lt $max_retries ]; do
+        # Execute the SQL file
+        psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/$unique_sql_file_name
+
+        # Check if the previous command succeeded
+        if [ $? -eq 0 ]; then
+          success=1
+          echo "SQL command executed successfully."
+          break
+        else
+          echo "Error encountered during SQL execution. Retrying..."
+          ((count++))
+          sleep 2 # wait before retrying
+        fi
+      done
+
+      if [ $success -eq 0 ]; then
+        echo "Failed to execute SQL command after $max_retries attempts."
+        exit 1
+      fi
     EOT
     environment = {
       render_template = templatefile("${path.module}/roles/${each.value.group_name}.sql", { groups = [for group in each.value.groups : lower(group)] })
