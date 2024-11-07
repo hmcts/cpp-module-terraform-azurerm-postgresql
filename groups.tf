581dca2c900903aca2cb3c1c0400d7cae6b1795c
Jyothi Balla <jyothi.balla@hmcts.net>
2024-11-07 15:51:55 +0000

    fix

--- groups.tf
+++ groups.tf
@@ -32,5 +32,4 @@ resource "azuread_group" "instance_groups" {
   for_each         = length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
   display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}${local.unique_suffix}${local.additional_segments}"
   security_enabled = true
-
 }
