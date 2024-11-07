8205f97429905bb2868774147a63062561561630
jyothi-balla <131781883+jyothi-balla@users.noreply.github.com>
2024-11-07 18:10:55 +0000

    Create unique group for server names having length more than 4  (#30)
    * Update groups.tf
    
    * Update groups.tf
    
    * Update groups.tf
    
    * test
    
    * Update flexible_server.tf
    
    * Update groups.tf
    
    * fix
    
    * fix
    
    * fix precommit
    
    * fix precommit

--- groups.tf
+++ groups.tf
@@ -7,10 +7,15 @@ locals {
   group_project          = upper(local.server_name_array[3])
   group_replica_id       = upper(trimprefix(local.server_name_array[2], "ccm"))
   rbac_platform          = var.platform == "nlv" ? "nle" : var.platform == "lv" ? "lve" : var.platform
+
+  // Create a unique identifier based on all segments after the standard format
+  additional_segments = upper(join("_", slice(local.server_name_array, 4, length(local.server_name_array))))
+  unique_suffix       = length(local.additional_segments) > 0 ? "_" : ""
+
   group_list = {
     for grp in var.entra_db_groups : "${var.server_name}-${lower(grp)}" => {
       groups = [
-        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}",
+        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}${local.unique_suffix}${local.additional_segments}",
         "GRP_PGFS_CP_${local.group_project}_${grp}",
         "GRP_PGFS_CP_${upper(local.rbac_platform)}_${grp}",
         "GRP_PGFS_CP_${local.group_environment_name}_${grp}"
@@ -24,8 +29,7 @@ locals {
 resource "azuread_group" "instance_groups" {
   // Group creation may break if server name does not meed expected format (e.g. replica DBs).
   // In such a case, no groups will be created..
-  for_each         = length(local.server_name_array) == 4 ? var.entra_db_groups : []
-  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}"
+  for_each         = length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
+  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}${local.unique_suffix}${local.additional_segments}"
   security_enabled = true
-
 }
