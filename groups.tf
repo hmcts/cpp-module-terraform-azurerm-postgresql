67d859af87402e2456d28f4a8294eab37e9e0061
Jyothi Balla <jyothi.balla@hmcts.net>
2024-11-11 16:40:35 +0000

    fix precommit

--- groups.tf
+++ groups.tf
@@ -29,7 +29,7 @@ locals {
 resource "azuread_group" "instance_groups" {
   // Group creation may break if server name does not meed expected format (e.g. replica DBs).
   // In such a case, no groups will be created..
-  for_each         = var.create_mode != "PointInTimeRestore"  && length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
+  for_each         = var.create_mode != "PointInTimeRestore" && length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
   display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}"
   security_enabled = true
 }
