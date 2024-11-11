52bb929c8607c4fb4da9645c663e153667f80dc5
Jyothi Balla <jyothi.balla@hmcts.net>
2024-11-11 12:47:24 +0000

    test

--- groups.tf
+++ groups.tf
@@ -8,14 +8,11 @@ locals {
   group_replica_id       = upper(trimprefix(local.server_name_array[2], "ccm"))
   rbac_platform          = var.platform == "nlv" ? "nle" : var.platform == "lv" ? "lve" : var.platform
 
-  // Create a unique identifier based on all segments after the standard format
-  additional_segments = upper(join("_", slice(local.server_name_array, 4, length(local.server_name_array))))
-  unique_suffix       = length(local.additional_segments) > 0 ? "_" : ""
 
   group_list = {
     for grp in var.entra_db_groups : "${var.server_name}-${lower(grp)}" => {
       groups = [
-        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}${local.unique_suffix}${local.additional_segments}",
+        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}",
         "GRP_PGFS_CP_${local.group_project}_${grp}",
         "GRP_PGFS_CP_${upper(local.rbac_platform)}_${grp}",
         "GRP_PGFS_CP_${local.group_environment_name}_${grp}"
@@ -30,6 +27,6 @@ resource "azuread_group" "instance_groups" {
   // Group creation may break if server name does not meed expected format (e.g. replica DBs).
   // In such a case, no groups will be created..
   for_each         = length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
-  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}${local.unique_suffix}${local.additional_segments}"
+  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}"
   security_enabled = true
 }
