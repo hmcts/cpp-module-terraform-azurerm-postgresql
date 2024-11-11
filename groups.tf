dc313534d34d5dd3d887a413e93fad05747b1a62
jyothi-balla <131781883+jyothi-balla@users.noreply.github.com>
2024-11-11 17:06:43 +0000

    Fix group creation (#31)
    * test
    
    * add data
    
    * fix
    
    * test
    
    * fix
    
    * Update groups.tf
    
    * fix precommit

--- groups.tf
+++ groups.tf
@@ -8,14 +8,11 @@ locals {
   group_replica_id       = upper(trimprefix(local.server_name_array[2], "ccm"))
   rbac_platform          = var.platform == "nlv" ? "nle" : var.platform == "lv" ? "lve" : var.platform
 
-  // Create a unique identifier based on all segments after the standard format
-  additional_segments = upper(join("_", slice(local.server_name_array, 4, length(local.server_name_array))))
-  unique_suffix       = length(local.additional_segments) > 0 ? "_" : ""
 
   group_list = {
     for grp in var.entra_db_groups : "${var.server_name}-${lower(grp)}" => {
       groups = [
-        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}${local.unique_suffix}${local.additional_segments}",
+        "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${grp}",
         "GRP_PGFS_CP_${local.group_project}_${grp}",
         "GRP_PGFS_CP_${upper(local.rbac_platform)}_${grp}",
         "GRP_PGFS_CP_${local.group_environment_name}_${grp}"
@@ -25,11 +22,14 @@ locals {
   }
 }
 
+
+
+
 // Groups that grant a specific permission to this specific PGFS instance
 resource "azuread_group" "instance_groups" {
   // Group creation may break if server name does not meed expected format (e.g. replica DBs).
   // In such a case, no groups will be created..
-  for_each         = length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
-  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}${local.unique_suffix}${local.additional_segments}"
+  for_each         = var.create_mode != "PointInTimeRestore" && length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
+  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}"
   security_enabled = true
 }
