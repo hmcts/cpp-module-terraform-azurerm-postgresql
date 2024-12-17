5007ebbe8b805fdc8ff36e1603636f28c00c9107
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-17 16:04:49 +0000

    test null

--- rbac.tf
+++ rbac.tf
@@ -21,24 +21,24 @@ resource "null_resource" "render_sql_files" {
 }
 
 resource "null_resource" "execute_sql_files" {
-#  triggers = {
-#    server_fqdn      = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
-#    az_user          = data.azuread_service_principal.current.client_id
-#    tenant           = data.azurerm_client_config.current.tenant_id
-#    client_id        = data.azurerm_key_vault_secret.entra_admin.0.value
-#    db_user          = var.entra_admin_user
-#    script_checksum  = templatefile("${path.module}/scripts/sql_role.sh", {})
-#  }
+  triggers = {
+    server_fqdn      = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
+    client_id          = data.azuread_service_principal.current.client_id
+    tenant_id           = data.azurerm_client_config.current.tenant_id
+    entra_admin        = data.azurerm_key_vault_secret.entra_admin.0.value
+    db_user          = var.entra_admin_user
+    script_checksum  = filemd5("${path.module}/scripts/sql_role.sh")
+  }
 
   provisioner "local-exec" {
     command = "bash ${path.module}/scripts/sql_role.sh"
     environment = {
       server_fqdn = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
-      az_user     = data.azuread_service_principal.current.client_id
-      tenant = data.azurerm_client_config.current.tenant_id
-      client_id = data.azurerm_key_vault_secret.entra_admin.0.value
+      client_id     = data.azuread_service_principal.current.client_id
+      tenant_id = data.azurerm_client_config.current.tenant_id
+      entra_admin = data.azurerm_key_vault_secret.entra_admin.0.value
       db_user = var.entra_admin_user
-      render_template = "${path.module}/roles/"
+      render_directory = "${path.module}/roles/"
     }
   }
 
--- scripts/sql_role.sh
+++ scripts/sql_role.sh
@@ -2,14 +2,14 @@
 set -e
 # Set PostgreSQL connection details
 
-az login --service-principal -u ${az_user} -t ${tenant} -p ${client_id}
+az login --service-principal -u ${client_id} -t ${tenant_id} -p ${entra_admin}
 export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
 
 
 # Find all SQL files in the folder and execute them sequentially
-for sql_file in $(ls ${render_template}/final_*.sql | sort); do
+for sql_file in $(ls ${render_directory}/final_*.sql | sort); do
     echo "Executing SQL file: $sql_file"
-    psql -h ${server_fqdn} -p 5432 -U ${db_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${render_template}/${sql_file}
+    psql -h ${server_fqdn} -p 5432 -U ${db_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${render_directory}/${sql_file}
     if [[ $? -ne 0 ]]; then
         echo "Error executing $sql_file. Exiting."
         exit 1
