75d8ff73314c4b1805c30211e862861eef997c5e
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-17 12:06:13 +0000

    test

--- rbac.tf
+++ rbac.tf
@@ -1,36 +1,21 @@
 resource "null_resource" "db_setup" {
-  count = length(values(local.group_list))
-
-  triggers = {
-    content = templatefile(
-      "${path.module}/roles/${values(local.group_list)[count.index].group_name}.sql",
-      { groups = [for group in values(local.group_list)[count.index].groups : lower(group)] }
-    )
-  }
-
   depends_on = [
     azurerm_postgresql_flexible_server.flexible_server,
     azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin,
     azuread_group.instance_groups
   ]
-  
+
   provisioner "local-exec" {
     command = <<EOT
-      unique_sql_file_name="final_${values(local.group_list)[count.index].group_name}_${local.group_project}.sql"
-      echo "$render_template" > ${path.module}/roles/$unique_sql_file_name
+      # Create a temporary file to store combined SQL content
+      combined_sql_file="${path.module}/roles/combined_setup_${local.group_project}.sql"
+      echo "${local.combined_sql_content}" > $combined_sql_file
+
+      # Execute the combined SQL script using psql
       az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
       export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
-      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/$unique_sql_file_name
+
+      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f $combined_sql_file
     EOT
-    environment = {
-      render_template = templatefile(
-        "${path.module}/roles/${values(local.group_list)[count.index].group_name}.sql",
-        { groups = [for group in values(local.group_list)[count.index].groups : lower(group)] }
-      )
-    }
   }
 }
-
-
-
-
