88b50f52eba54dcd9fdacf18291eba5f77fe9906
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-17 15:21:38 +0000

    test null

--- scripts/sql_role.sh
+++ scripts/sql_role.sh
@@ -9,7 +9,7 @@ export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --quer
 # Find all SQL files in the folder and execute them sequentially
 for sql_file in $(ls ${render_template}/final_*.sql | sort); do
     echo "Executing SQL file: $sql_file"
-    psql -h ${server_fqdn} -p 5432 -U ${DB_USER} -d postgres -v 'ON_ERROR_STOP=1' -f ${render_template}/${sql_file}
+    psql -h ${server_fqdn} -p 5432 -U ${db_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${render_template}/${sql_file}
     if [[ $? -ne 0 ]]; then
         echo "Error executing $sql_file. Exiting."
         exit 1
