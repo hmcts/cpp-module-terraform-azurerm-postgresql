7dbf1e6f495cf7b990ae86101c49621462b085a0
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-17 16:54:59 +0000

    test null

--- rbac.tf
+++ rbac.tf
@@ -21,25 +21,25 @@ resource "null_resource" "render_sql_files" {
 }
 
 resource "null_resource" "execute_sql_files" {
-  triggers = {
-    server_fqdn      = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
-    client_id          = data.azuread_service_principal.current.client_id
-    tenant_id           = data.azurerm_client_config.current.tenant_id
-    entra_admin        = data.azurerm_key_vault_secret.entra_admin.0.value
-    db_user          = var.entra_admin_user
-    script_checksum  = filemd5("${path.module}/scripts/sql_role.sh")
-  }
+
 
   provisioner "local-exec" {
-    command = "bash ${path.module}/scripts/sql_role.sh"
-    environment = {
-      server_fqdn = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
-      client_id     = data.azuread_service_principal.current.client_id
-      tenant_id = data.azurerm_client_config.current.tenant_id
-      entra_admin = data.azurerm_key_vault_secret.entra_admin.0.value
-      db_user = var.entra_admin_user
-      render_directory = "${path.module}/roles"
-    }
+    command = <<EOT
+      # Log in to Azure using the service principal
+      az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
+
+      # Get the PostgreSQL access token from Azure
+      export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
+
+      # Loop through all .sql files in the roles directory and execute them one by one
+      for sql_file in $(ls ${path.module}/roles/final_*.sql | sort); do
+          echo "Executing SQL file: $sql_file"
+
+          # Execute the SQL file using psql
+         psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/$sql_file
+
+      done
+    EOT
   }
 
   depends_on = [null_resource.render_sql_files]
