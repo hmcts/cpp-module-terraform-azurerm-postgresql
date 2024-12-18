b3c6831baebdbb89c783b6d6ae02e7d67bebafa2
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-18 15:10:04 +0000

    test null

--- rbac.tf
+++ rbac.tf
@@ -42,7 +42,7 @@ resource "null_resource" "execute_sql_files" {
       tenant_id = data.azurerm_client_config.current.tenant_id
       entra_admin = data.azurerm_key_vault_secret.entra_admin.0.value
       db_user = var.entra_admin_user
-      file_name = [for item in local.group_list : item.group_name]
+      groups = join(",", [for item in local.group_list : item.group_name])
       group_project = local.group_project
     }
   }
--- scripts/sql_role.sh
+++ scripts/sql_role.sh
@@ -6,9 +6,19 @@ az login --service-principal -u ${client_id} -t ${tenant_id} -p ${entra_admin}
 export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
 
 
-# Find all SQL files in the folder and execute them sequentially
-for sql_file in $(ls ${file_name} | sort); do
-    echo "Executing SQL file: $sql_file"
-    psql -h ${server_fqdn} -p 5432 -U ${db_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${sql_file}
+
+IFS=',' read -r -a group_array <<< "$groups"
+for group in "${group_array[@]}"; do
+  sql_file="final_${group}_${group_project}.sql"
+
+  # Check if the file exists and loop through it
+  if [[ -f "${path_module}/roles/${sql_file}" ]]; then
+    echo "Executing SQL file: ${sql_file}"
+    psql -h "${server_fqdn}" -p 5432 -U "${db_user}" -d postgres -v 'ON_ERROR_STOP=1' -f "${path_module}/roles/${sql_file}"
+  fi
 done
+
+
+
+
 
