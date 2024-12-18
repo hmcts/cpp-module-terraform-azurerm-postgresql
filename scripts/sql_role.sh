6c7483a7fe165fb5ad39f4a20a9eccda41095275
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-18 16:29:34 +0000

    test null

--- scripts/sql_role.sh
+++ scripts/sql_role.sh
@@ -10,12 +10,9 @@ export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --quer
 IFS=',' read -r -a group_array <<< "$groups"
 for group in "${group_array[@]}"; do
   sql_file="final_${group}_${group_project}.sql"
+  echo "Executing SQL file: ${sql_file}" >> /tmp/sql_role_debug.log
+  psql -h "${server_fqdn}" -p 5432 -U "${db_user}" -d postgres -v 'ON_ERROR_STOP=1' -f "${path_module}/roles/${sql_file}" >> /tmp/sql_role_debug.log 2>&1
 
-  # Check if the file exists and loop through it
-  if [[ -f "${path_module}/roles/${sql_file}" ]]; then
-    echo "Executing SQL file: ${sql_file}" >> /tmp/sql_role_debug.log
-    psql -h "${server_fqdn}" -p 5432 -U "${db_user}" -d postgres -v 'ON_ERROR_STOP=1' -f "${path_module}/roles/${sql_file}" >> /tmp/sql_role_debug.log 2>&1
-  fi
 done
 
 
