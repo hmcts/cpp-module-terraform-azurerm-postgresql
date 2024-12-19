7e45f7b7bb71aa0c2a809f73698c0c20fcdd9161
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-19 16:35:48 +0000

    test null

--- rbac.tf
+++ rbac.tf
@@ -9,14 +9,18 @@ resource "null_resource" "render_sql_files" {
     azurerm_postgresql_flexible_server.flexible_server,
     azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin,
     azuread_group.instance_groups
+
   ]
 
   provisioner "local-exec" {
     command = <<EOT
       unique_sql_file_name="final_${each.value.group_name}_${local.group_project}.sql"
       echo "$render_template" > ${path.module}/roles/$unique_sql_file_name
+      az login --service-principal -u ${data.azuread_service_principal.current.client_id} -t ${data.azurerm_client_config.current.tenant_id} -p ${data.azurerm_key_vault_secret.entra_admin.0.value}
+      export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query "[accessToken]" -o tsv)
+      psql -h ${azurerm_postgresql_flexible_server.flexible_server.0.fqdn} -p 5432 -U ${var.entra_admin_user} -d postgres -v 'ON_ERROR_STOP=1' -f ${path.module}/roles/$unique_sql_file_name
     EOT
-
+    max_retries = 4
     environment = {
       render_template = templatefile("${path.module}/roles/${each.value.group_name}.sql", { groups = [for group in each.value.groups : lower(group)] })
     }
@@ -24,32 +28,32 @@ resource "null_resource" "render_sql_files" {
 }
 
 
-resource "null_resource" "execute_sql_files" {
-  triggers = {
-    content = jsonencode({
-      for group in local.group_list : group.group_name => templatefile("${path.module}/roles/${group.group_name}.sql", { groups = [for g in group.groups : lower(g)] })
-    })
-  }
-
-  provisioner "local-exec" {
-    command = "bash -x ${path.module}/scripts/sql_role.sh"
-    environment = {
-      server_fqdn   = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
-      client_id     = data.azuread_service_principal.current.client_id
-      tenant_id     = data.azurerm_client_config.current.tenant_id
-      entra_admin   = data.azurerm_key_vault_secret.entra_admin.0.value
-      db_user       = var.entra_admin_user
-      file_path     = "${path.module}/roles"
-      groups        = join(",", [for item in local.group_list : item.group_name])
-      group_project = local.group_project
-    }
-    on_failure = fail
-  }
-
-  depends_on = [
-    azurerm_postgresql_flexible_server.flexible_server,
-    azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin,
-    azuread_group.instance_groups,
-    null_resource.render_sql_files
-  ]
-}
+#resource "null_resource" "execute_sql_files" {
+#  triggers = {
+#    content = jsonencode({
+#      for group in local.group_list : group.group_name => templatefile("${path.module}/roles/${group.group_name}.sql", { groups = [for g in group.groups : lower(g)] })
+#    })
+#  }
+#
+#  provisioner "local-exec" {
+#    command = "bash -x ${path.module}/scripts/sql_role.sh"
+#    environment = {
+#      server_fqdn   = azurerm_postgresql_flexible_server.flexible_server.0.fqdn
+#      client_id     = data.azuread_service_principal.current.client_id
+#      tenant_id     = data.azurerm_client_config.current.tenant_id
+#      entra_admin   = data.azurerm_key_vault_secret.entra_admin.0.value
+#      db_user       = var.entra_admin_user
+#      file_path     = "${path.module}/roles"
+#      groups        = join(",", [for item in local.group_list : item.group_name])
+#      group_project = local.group_project
+#    }
+#    on_failure = fail
+#  }
+#
+#  depends_on = [
+#    azurerm_postgresql_flexible_server.flexible_server,
+#    azurerm_postgresql_flexible_server_active_directory_administrator.entra_admin,
+#    azuread_group.instance_groups,
+#    null_resource.render_sql_files
+#  ]
+#}
