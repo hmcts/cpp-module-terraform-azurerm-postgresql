5960bed1ac8cd7b26b32fd28fa58b5a14f12a537
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-19 20:42:43 +0000

    test null

--- roles/editor.sql
+++ roles/editor.sql
@@ -13,7 +13,6 @@ $do$;
 --Editor privileges:
 grant select, insert, update on all tables in schema public to ${group};
 grant usage, select, update on all sequences in schema public to ${group};
-
 alter default privileges in schema public grant select, insert, update on tables to ${group};
 alter default privileges in schema public grant usage, select, update on sequences to ${group};
 -- Set pgaudit log level to groups:
