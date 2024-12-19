ac74ac9b930849fb2ec771ca4814c3662d1cc53d
Jyothi Balla <jyothi.balla@hmcts.net>
2024-12-19 09:44:57 +0000

    test null

--- scripts/sql_role.sh
+++ scripts/sql_role.sh
@@ -12,7 +12,7 @@ for group in "${group_array[@]}"; do
   sql_file="final_${group}_${group_project}.sql"
   if [ -f "${file_path}/${sql_file}" ]; then
     echo "Executing SQL file: ${sql_file}" >> /tmp/sql_role_debug.log
-    psql -h "${server_fqdn}" -p 5432 -U "${db_user}" -d postgres -v 'ON_ERROR_STOP=1' -f "${file_path}/${sql_file}" >> /tmp/sql_role_debug.log 2>&1
+    psql -h "${server_fqdn}" -p 5432 -U "${db_user}" -d postgres -v 'ON_ERROR_STOP=1' -f "${file_path}/${sql_file}"
   else
     echo "File ${sql_file} does not exist, skipping..." >> /tmp/sql_role_debug.log
   fi
