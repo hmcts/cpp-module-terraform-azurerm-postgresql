c92f2a81f8c0aad0dc064c6e016c1f89355d85d2
Abhivan Chekuri <abhivan.chekuri1@hmcts.net>
2024-12-20 10:37:17 +0000

    DTSPO-22315 testing moving AD group creation to root

--- groups.tf
+++ groups.tf
@@ -8,7 +8,6 @@ locals {
   group_replica_id       = replace(local.server_name_array[2], "/[^\\d]/", "")
   rbac_platform          = var.platform == "nlv" ? "nle" : var.platform == "lv" ? "lve" : var.platform
 
-
   group_list = {
     for grp in var.entra_db_groups : "${var.server_name}-${lower(grp)}" => {
       groups = [
@@ -20,13 +19,4 @@ locals {
       group_name = lower(grp)
     }
   }
-}
-
-// Groups that grant a specific permission to this specific PGFS instance
-resource "azuread_group" "instance_groups" {
-  // Group creation may break if server name does not meed expected format (e.g. replica DBs).
-  // In such a case, no groups will be created..
-  for_each         = var.create_mode != "PointInTimeRestore" && length(var.entra_db_groups) > 0 ? var.entra_db_groups : []
-  display_name     = "GRP_PGFS_CP_${local.group_environment_name}_${local.group_project}_${local.group_replica_id}_${each.key}"
-  security_enabled = true
 }
